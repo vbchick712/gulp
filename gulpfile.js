d664ad66267c61f43cb95aef22583becf59c1b3e
vbchick712 <shannan.muench@gmail.com>
2017-10-05 13:45:09 -0400

    refactored gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,66 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    coffee = require('gulp-coffee'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect');
+
+var coffeeSources = ['scripts/hello.coffee'],
+    jsSources = ['scripts/*.js'],
+    sassSources = ['styles/*.scss'],
+    htmlSources = ['**/*.html'],
+    outputDir = 'assets';
+
+gulp.task('log', function() {
+  gutil.log('=== My First Task ===')
+});
 
 gulp.task('copy', function() {
   gulp.src('index.html')
-  .pipe(gulp.dest('assets'))
+  .pipe(gulp.dest('outputDir'))
 });
-
-var gutil = require('gulp-util');
-
-gulp.task('log', function() {
-  gutil.log('== My Log Task ==')
-});
-
-var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  gulp.src('styles/main.css')
+  gulp.src(sassSources)
   .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
-  .pipe(gulp.dest('assests'))
+  .pipe(gulp.dest('assets'))
+  .pipe(connect.reload())
 });
 
-var coffee = require('gulp-coffee');
-
 gulp.task('coffee', function() {
-  gulp.src('scripts/hello.coffee')
+  gulp.src(coffeeSources)
   .pipe(coffee({bare: true})
     .on('error', gutil.log))
   .pipe(gulp.dest('scripts'))
 });
 
-var uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
-
 gulp.task('js', function() {
-  gulp.src('scripts/*.js')
+  gulp.src(jsSources)
   .pipe(uglify())
   .pipe(concat('script.js'))
-  .pipe(gulp.dest('assets'))
+  .pipe(gulp.dest(outputDir))
+  .pipe(connect.reload())
 });
 
-gulp.task('default', ['coffee', 'js']);
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['sass']);
+  gulp.watch(htmlSources), ['html'];
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'coffee', 'js', 'sass', 'connect', 'watch']);
